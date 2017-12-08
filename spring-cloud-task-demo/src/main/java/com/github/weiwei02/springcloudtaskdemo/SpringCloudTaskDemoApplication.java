2fe1ff6f2d53631825d5d1951843e97f1496e3d8
weiwei <weiwei02@vip.qq.com>
2017-12-09 02:02:10 +0800

    床架项目基本架构

---
+++ spring-cloud-task-demo/src/main/java/com/github/weiwei02/springcloudtaskdemo/SpringCloudTaskDemoApplication.java
@@ -0,0 +1,12 @@
+package com.github.weiwei02.springcloudtaskdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudTaskDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudTaskDemoApplication.class, args);
+	}
+}
---
+++ spring-cloud-task-demo/src/test/java/com/github/weiwei02/springcloudtaskdemo/SpringCloudTaskDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.weiwei02.springcloudtaskdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudTaskDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
