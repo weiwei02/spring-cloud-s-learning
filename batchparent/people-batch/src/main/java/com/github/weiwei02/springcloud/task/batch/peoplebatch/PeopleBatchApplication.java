f8b6fc58ae03d668f9786751f804522a86727e73
weiwei <weiwei02@vip.qq.com>
2017-12-11 22:58:16 +0800

    增加people-batch项目

--- batchparent/people-batch/src/main/java/com/github/weiwei02/springcloud/task/batch/peoplebatch/PeopleBatchApplication.java
+++ batchparent/people-batch/src/main/java/com/github/weiwei02/springcloud/task/batch/peoplebatch/PeopleBatchApplication.java
@@ -1,9 +1,11 @@
-package com.github.weiwei02.peoplebatch;
+package com.github.weiwei02.springcloud.task.batch.peoplebatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class PeopleBatchApplication {
 
 	public static void main(String[] args) {
