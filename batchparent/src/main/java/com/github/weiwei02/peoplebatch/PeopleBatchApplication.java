4e43573eb9657112fa66cddc64523ba6cbebeb12
weiwei <weiwei02@vip.qq.com>
2017-12-11 22:36:15 +0800

    增加people-batch项目

---
+++ batchparent/src/main/java/com/github/weiwei02/peoplebatch/PeopleBatchApplication.java
@@ -0,0 +1,12 @@
+package com.github.weiwei02.peoplebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleBatchApplication.class, args);
+	}
+}
